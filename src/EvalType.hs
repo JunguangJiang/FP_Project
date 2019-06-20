5673d4764d82cf988bc00f9933969800e2cf4f62
JunguangJiang <13126830206@163.com>
2019-06-20 11:35:40 +0800

    finish ast types

--- src/EvalType.hs
+++ src/EvalType.hs
@@ -5,6 +5,7 @@ import AST
 import Control.Monad.State
 import Text.Printf
 import Data.Map.Strict as M
+import Control.Monad
 
 -- type TEnv = Map String Type
 
@@ -16,7 +17,12 @@ data Context = Context { -- 可以用某种方式定义上下文，用于记录�
 type ContextState a = StateT Context Maybe a
 
 withVars :: Map String Type -> ContextState a -> ContextState a
-withVars = undefined
+withVars map op = do
+    env <- get
+    modify $ \s -> env {getVars = M.union map (getVars s)}
+    r <- op
+    put env
+    return r
 
 withVar :: String -> Type -> ContextState a -> ContextState a
 withVar x t op = do
@@ -85,31 +91,59 @@ evalApply = undefined
 getADTTypes :: [ADT] -> Map String Type
 getADTTypes = undefined
 
-getADTCtors :: [ADT] -> Map String ([Type], String)
-getADTCtors = undefined
+getADTCtor :: ADT -> Map String ([Type], String)
+getADTCtor (ADT adtName conss) = Prelude.foldl (\upd (cons, types) -> M.insert cons (types, adtName) upd) M.empty conss
 
-matchPT :: Pattern -> Type -> Map String ([Type], String) -> Maybe (Map String Type)
-matchPT = undefined
+getADTCtors :: [ADT] -> Map String ([Type], String)
+getADTCtors = Prelude.foldl (\upd adt -> M.union upd (getADTCtor adt)) M.empty
+
+matchPT :: Pattern -> Type -> M.Map String ([Type], String) -> Maybe (M.Map String Type)
+matchPT (PBoolLit _) t _ = if t == TBool then return M.empty else Nothing
+matchPT (PIntLit _)  t _ = if t == TInt then return M.empty else Nothing
+matchPT (PCharLit _) t _ = if t == TChar then return M.empty else Nothing
+matchPT (PVar s) t _ = return $ M.fromList [(s, t)]
+matchPT (PData con patterns) t ctor = do
+    (types, adtName) <- ctor M.!? con
+    let (TData adtName') = t
+    if adtName == adtName' && length patterns == length types then foldM 
+        (\upd (p', t') -> do
+            map <- matchPT p' t' ctor
+            return $ M.union upd map
+        ) M.empty (zip patterns types)
+    else Nothing
 
 evalOneCase :: Type -> (Pattern, Expr) -> ContextState Type
-evalOneCase = undefined
+evalOneCase t (p, e) = do
+    env <- get 
+    case matchPT p t (getCtors env) of
+        Just vars -> withVars vars $ eval e
+        _ -> lift Nothing
 
 evalCase :: Expr -> [(Pattern, Expr)] -> ContextState Type
-evalCase = undefined
+evalCase e pes = do
+    t <- eval e
+    types <- foldM (\upd pe -> do
+            t' <- evalOneCase t pe
+            return (t' : upd)
+        ) [] pes
+    let first_type = head types
+    if all (==first_type) types then return first_type else lift Nothing
 
 evalEq :: Expr -> Expr -> ContextState Type
-evalEq = undefined
+evalEq e1 e2 = do
+    isType e1 [TBool, TInt, TChar]
+    isSameType e1 e2
 
 evalOrd :: Expr -> Expr -> ContextState Type
-evalOrd = undefined
+evalOrd e1 e2 = do
+    isType e1 [TInt, TChar]
+    isSameType e1 e2
 
 eval :: Expr -> ContextState Type
 eval (EBoolLit _) = return TBool
 eval (EIntLit _) = return TInt
 eval (ECharLit _) = return TChar
-eval (ENot e) = do 
-    isBool e 
-    return TBool
+eval (ENot e) = isBool e 
 
 eval (EAnd e1 e2) = do
     isBool e1
@@ -122,24 +156,12 @@ eval (ESub e1 e2) = areTwoInts e1 e2
 eval (EMul e1 e2) = areTwoInts e1 e2
 eval (EDiv e1 e2) = areTwoInts e1 e2
 eval (EMod e1 e2) = areTwoInts e1 e2
-eval (EEq e1 e2) = do
-    isType e1 [TBool, TInt, TChar]
-    isSameType e1 e2
-eval (ENeq e1 e2) = do
-    isType e1 [TBool, TInt, TChar]
-    isSameType e1 e2
-eval (ELt e1 e2) = do 
-    isType e1 [TInt, TChar]
-    isSameType e1 e2
-eval (EGt e1 e2) = do
-    isType e1 [TInt, TChar]
-    isSameType e1 e2
-eval (ELe e1 e2) = do
-    isType e1 [TInt, TChar]
-    isSameType e1 e2
-eval (EGe e1 e2) = do
-    isType e1 [TInt, TChar]
-    isSameType e1 e2
+eval (EEq e1 e2) = evalEq e1 e2
+eval (ENeq e1 e2) = evalEq e1 e2
+eval (ELt e1 e2) = evalOrd e1 e2
+eval (EGt e1 e2) = evalOrd e1 e2
+eval (ELe e1 e2) = evalOrd e1 e2
+eval (EGe e1 e2) = evalOrd e1 e2
 
 eval (EIf e1 e2 e3) = do
     t1 <- eval e1
@@ -163,7 +185,9 @@ eval (EVar x) = do
     env <- get 
     case getVars env M.!? x of
         Just t -> return t
-        Nothing -> lift Nothing  
+        Nothing -> case getCtors env M.!? x of
+            (Just (types, adtName)) -> return $ Prelude.foldr TArrow (TData adtName) types
+            Nothing -> lift Nothing
 
 eval (EApply e1 e2) = do
     t1 <- eval e1
@@ -171,7 +195,8 @@ eval (EApply e1 e2) = do
     case t1 of
         TArrow t11 t12 -> if t11 == t2 then return t12 else lift Nothing
         _ -> lift Nothing
-eval _ = undefined
+
+eval (ECase e pes) = evalCase e pes
 
 evalType :: Program -> Maybe Type
-evalType (Program adts body) = evalStateT (eval body) $ Context{getVars = M.empty, getCtors = M.empty}
+evalType (Program adts body) = evalStateT (eval body) $ Context{getVars = M.empty, getCtors = getADTCtors adts}
